c86b73f6604f417f316e618769db77e8c46072fa
Victor Ezealor <75592515+uzochukwuV@users.noreply.github.com>
2024-11-17 16:53:46 -0800

    initail commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hero from './components/Hero'
+
+function App() {
+  
+
+  return (
+    <>
+     <Hero />
+    </>
+  )
+}
+
+export default App
